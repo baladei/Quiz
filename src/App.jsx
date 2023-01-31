442cd9cd1be76d700def64d618533f641bd8eb81
Fernando Baladei <f.baladei@gmail.com>
2023-01-31 06:15:30 -0300

    finished Welcome page, almost complete Questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { Welcome } from './components/Welcome'
+import { Questions } from './components/Questions'
+import { QuizContext } from './context/quiz'
+
+import { useContext } from 'react'
 
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const [quizState, dispatch ] = useContext(QuizContext)
+
+  useEffect(()=> {
+    //embaralhar as perguntas
+    dispatch({type: "REORDER_QUESTIONS"})
+  } , [])
+
 
   return (
     <div className="App">
-      <h3>Quiz de Programação</h3>
-      <Welcome />
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Questions />}
+      {quizState.gameStage === "End" && <Welcome />}
     </div>
   )
 }
